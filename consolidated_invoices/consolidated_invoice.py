4c929fc00d06aceaba5fcb8004ec31ff668bd444
Colin Newell <colin@opusvl.com>
2014-06-09 16:02:19 +0100

    Started new data model

---
+++ consolidated_invoices/consolidated_invoice.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+from openerp.osv import fields, osv, orm
+
+
+class consolidated_invoice(osv.osv):
+
+    _name = 'account.consolidated.invoice'
+    #_inherit = []
+    _description = 'Consolidated invoice'
+    _order = "id desc"
+    _columns = {
+        'name': fields.char('Reference', size=64, select=True, readonly=True, states={'draft': [('readonly',False)]}),
+        'reference': fields.char('Invoice Reference', size=64, help="The partner reference of this invoice."),
+        'invoice_ids': fields.one2many('account.invoice', 'consolidated_invoice_id', 'Invoices', readonly=True, states={'draft':[('readonly',False)]}),
+        'comment': fields.text('Additional Information'),
+        'state': fields.selection([
+            ('draft','Draft'),
+            ('open','Open'),
+            ('paid','Paid'),
+            ('cancel','Cancelled'),
+            ],'Status', select=True, readonly=True, track_visibility='onchange',
+            help=' * The \'Draft\' status is used when a user is encoding a new and unconfirmed Invoice. \
+            \n* The \'Open\' status is used when user create invoice,a invoice number is generated.Its in open status till user does not pay invoice. \
+            \n* The \'Paid\' status is set automatically when the invoice is paid. Its related journal entries may or may not be reconciled. \
+            \n* The \'Cancelled\' status is used when user cancel invoice.'),
+        'date_invoice': fields.date('Invoice Date', readonly=True, states={'draft':[('readonly',False)]}, select=True, help="Keep empty to use the current date"),
+        'company_id': fields.many2one('res.company', 'Company', required=True, change_default=True, readonly=True, states={'draft':[('readonly',False)]}),
+    }
+    _defaults = {
+        'state': 'draft',
+        'company_id': lambda self,cr,uid,c: self.pool.get('res.company')._company_default_get(cr, uid, 'account.invoice', context=c),
+    }
+
+
+class account_invoice(osv.osv):
+
+    _inherit = "account.invoice"
+
+    _columns = {
+        'consolidated_invoice_id': fields.many2one('account.consolidated.invoice', 'Consolidated Invoice Reference', select=True),
+    }

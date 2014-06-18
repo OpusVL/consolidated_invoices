6e7aaf146c48d6e9ab8c352778e1ec84b62ff8c9
Colin Newell <colin.newell@gmail.com>
2014-06-18 15:32:35 +0100

    Started to fill in code

--- consolidated_invoices/wizard/invoice_selection.py
+++ consolidated_invoices/wizard/invoice_selection.py
@@ -26,3 +26,28 @@ class invoice_merge(orm.TransientModel):
     _name = "consolidated.invoice.selection"
     _description = "Consolidated Invoice Selection"
 
+    def fields_view_get(self, cr, uid, view_id=None, view_type='form',
+                        context=None, toolbar=False, submenu=False):
+        if context is None:
+            context = {}
+        # FIXME: implement validation of the invoices here.
+        res = super(invoice_merge, self).fields_view_get(cr, uid, view_id=view_id, view_type=view_type, context=context, toolbar=toolbar, submenu=False)
+        return res
+
+    def create_consolidated_invoice(self, cr, uid, ids, context=None):
+        # FIXME: create the new consolidated invoice.
+        mod_obj = self.pool.get('ir.model.data')
+        if context is None:
+            context = {}
+        result = mod_obj._get_id(cr, uid, 'account', 'invoice_form')
+        id = mod_obj.read(cr, uid, result, ['res_id'])
+        return {
+            'domain': "[('id','in', [" + ','.join(map(str, allinvoices.keys())) + "])]",
+            'name': _('Consolidated Invoice'),
+            'view_type': 'form',
+            'view_mode': 'tree,form',
+            'res_model': 'account.consolidated.invoice',
+            'view_id': False,
+            'type': 'ir.actions.act_window',
+            'search_view_id': id['res_id']
+        }

32472ee4f77e9810fc47a92eac4d02e3ac074c7c
Colin Newell <colin@opusvl.com>
2014-06-20 13:27:47 +0100

    Started to add code for creation button

--- consolidated_invoices/wizard/consolidate.py
+++ consolidated_invoices/wizard/consolidate.py
@@ -44,5 +44,22 @@ class consolidator(orm.TransientModel):
         defaults.update({ 'partner_id': partner_id })
         return defaults
 
-    def consolidate_invoices(self, cr, uid, ids, context=None):
-        pass
+    def consolidate_invoictes(self, cr, uid, ids, context=None):
+        mod_obj = self.pool.get('ir.model.data')
+        if context is None:
+            context = {}
+        result = mod_obj._get_id(cr, uid, 'consolidated_invoices', 'consolidated_invoice_form')
+        id = mod_obj.read(cr, uid, result, ['res_id'])
+        consolidated_invoice_obj = self.pool.get('account.consolidated.invoice')
+        #invoice_id = consolidated_invoice_obj.create_for_invoices(cr, uid, invoice_ids, context=context)
+        invoice_id = 1
+        return {
+            'name': _('Consolidated Invoice'),
+            'view_type': 'form',
+            'view_mode': 'form',
+            'view_id': id['res_id'],
+            'res_model': 'account.consolidated.invoice',
+            'res_id': invoice_id,
+            'target': 'current',
+            'type': 'ir.actions.act_window',
+        }
